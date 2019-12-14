02f6f998cabe02994ddd79d325e6d35f27f30bd0
Francisco  Inoque <40237204+frantchessico@users.noreply.github.com>
2019-12-14 16:35:07 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,14 @@
+
 export const environment = {
-  production: true, 
+  production: true,
   firebase: {
-    apiKey: "AIzaSyCwZbNlQi4ofv-JoiNmOY7er_2CfJCFvhE",
-    authDomain: "gestion-client-aeaee.firebaseapp.com",
-    databaseURL: "https://gestion-client-aeaee.firebaseio.com",
-    projectId: "gestion-client-aeaee",
-    storageBucket: "gestion-client-aeaee.appspot.com",
-    messagingSenderId: "1000826572238"
+    apiKey: "#",
+    authDomain: "#",
+    databaseURL: "#",
+    projectId: "#",
+    storageBucket: "#",
+    messagingSenderId: "#",
+    appId: "1#"
   }
 };
+
